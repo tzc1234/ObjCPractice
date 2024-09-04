ab1a23c0f1fac7f02ced4f91d00db8c2f031b3ca
tzc1234 <peopli9997@gmail.com>
2024-09-04 17:24:52 +0100

    Add Photo

---
+++ ObjCPractice/Networking/Photo.h
@@ -0,0 +1,20 @@
+//
+//  Photo.h
+//  ObjCPractice
+//
+//  Created by Tsz-Lung on 04/09/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Photo : NSObject
+    
+@property (nonatomic, readonly, nonnull) NSString *ID, *author;
+@property (nonatomic, readonly, nonnull) NSURL *webURL, *url;
+@property (nonatomic, readonly) NSInteger width, height;
+
+- (nonnull instancetype)init __attribute__((unavailable("This method is unavailable.")));
+- (nonnull instancetype)initWithID:(nonnull NSString *)ID author:(nonnull NSString *)author width:(NSInteger)width
+                           height:(NSInteger)height webURL:(nonnull NSURL *)webURL url:(nonnull NSURL *)url;
+
+@end
---
+++ ObjCPractice/Networking/Photo.m
@@ -0,0 +1,24 @@
+//
+//  Photo.m
+//  ObjCPractice
+//
+//  Created by Tsz-Lung on 04/09/2024.
+//
+
+#import "Photo.h"
+
+@implementation Photo
+
+-(instancetype)initWithID:(NSString *)ID author:(NSString *)author width:(NSInteger)width
+                   height:(NSInteger)height webURL:(NSURL *)webURL url:(NSURL *)url {
+    self = [super init];
+    _ID = [ID copy];
+    _author = [author copy];
+    _width = width;
+    _height = height;
+    _webURL = [webURL copy];
+    _url = [url copy];
+    return self;
+}
+
+@end
