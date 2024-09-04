1a195ca1d9f190510f6521a81ffb479634eabb82
tzc1234 <peopli9997@gmail.com>
2024-09-04 16:09:55 +0100

    Add title for ImageViewController

--- ObjCPractice/UI/View Controllers/ImagesViewController.m
+++ ObjCPractice/UI/View Controllers/ImagesViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.title = @"Images";
     self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
     [self.tableView registerClass:[ImageCell class] forCellReuseIdentifier:ImageCell.cellID];
 }
