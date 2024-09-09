ddfd97acfdc44994b6a4fd970259ee69d166ee32
tzc1234 <peopli9997@gmail.com>
2024-09-09 15:28:43 +0100

    Composite with ImageDataCacher in order to cache the image data in memory

--- ObjCPractice/Main/SceneDelegate.m
+++ ObjCPractice/Main/SceneDelegate.m
@@ -10,6 +10,10 @@
 #import "RemotePhotosLoader.h"
 #import "RemoteImageDataLoader.h"
 #import "PhotosViewComposer.h"
+#import "NSCacheImageDataStore.h"
+#import "ImageDataCacher.h"
+#import "ImageDataLoaderCacheDecorator.h"
+#import "ImageDataLoaderWithFallbackComposite.h"
 
 @implementation SceneDelegate
 
@@ -26,11 +30,21 @@
     NSURL *url = [[NSURL alloc] initWithString:@"https://picsum.photos/v2/list"];
     URLSessionHTTPClient *client = [[URLSessionHTTPClient alloc] initWithSession:NSURLSession.sharedSession];
     RemotePhotosLoader *loader = [[RemotePhotosLoader alloc] initWithURL:url client:client];
+    
     RemoteImageDataLoader *imageDataLoader = [[RemoteImageDataLoader alloc] initWithClient:client];
+    NSCacheImageDataStore *imageDataStore = [[NSCacheImageDataStore alloc] init];
+    ImageDataCacher *imageCacher = [[ImageDataCacher alloc] initWith:imageDataStore];
+    
+    ImageDataLoaderCacheDecorator *imageDataLoaderWithCache = [[ImageDataLoaderCacheDecorator alloc]
+                                                               initWithLoader:imageDataLoader
+                                                               andCacher:imageCacher];
+    ImageDataLoaderWithFallbackComposite *imageDataLoaderWithFallback = [[ImageDataLoaderWithFallbackComposite alloc]
+                                                                         initWithPrimary:imageCacher
+                                                                         andFallback:imageDataLoaderWithCache];
     
     window.rootViewController = [[UINavigationController alloc] initWithRootViewController:
                                  [PhotosViewComposer composeWithPhotoLoader:loader
-                                                            imageDataLoader:imageDataLoader]];
+                                                            imageDataLoader:imageDataLoaderWithFallback]];
     [window makeKeyAndVisible];
 }
 
